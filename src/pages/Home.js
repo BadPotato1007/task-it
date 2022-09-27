15008d3ec0e17afd861716dcbf6d681493e17ca7
Neerav Gupta <70943638+TheMysteryOfCode@users.noreply.github.com>
2022-09-27 12:40:02 +0530

    modified:   src/pages/Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
 import HomeMainSection from "./sections/HomeMainSection";
-
+import Navbar from "../components/Navbar";
 export default function Home() {
   return (
     <div>
+      <Navbar/>
       <HomeMainSection />
     </div>
   );
