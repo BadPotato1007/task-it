c56994c6277974be58a972ffde96cc95b6c65f1b
Neerav Gupta <70943638+TheMysteryOfCode@users.noreply.github.com>
2022-10-05 20:12:05 +0530

    new file:   build/static/css/main.5f7cf872.chunk.css.map

---
+++ build/precache-manifest.960f363c51cfebcb6ef10e3e92ae931f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86a3197a74fd30f57df7f481ffb6aab4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ff2c1b2169fe1265a14",
+    "url": "/static/css/main.5f7cf872.chunk.css"
+  },
+  {
+    "revision": "5a7ae2211b83ee810f5a",
+    "url": "/static/js/2.7cd4a46b.chunk.js"
+  },
+  {
+    "revision": "1fb100bc1864f6e090d8847f7f6b1a0f",
+    "url": "/static/js/2.7cd4a46b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ff2c1b2169fe1265a14",
+    "url": "/static/js/main.9df7c20f.chunk.js"
+  },
+  {
+    "revision": "a07415f3a612dc5038b4",
+    "url": "/static/js/runtime-main.2636bc25.js"
+  }
+]);
